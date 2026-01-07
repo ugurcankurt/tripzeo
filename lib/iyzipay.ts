53d127e84f2506a75cb46af173a6f6c585fe43fb
Uğur Cankurt <ugurgraphic@gmail.com>
2026-01-07 03:36:32 +0300

    fix(build): Add fallback URI for Iyzipay to prevent build failure

--- lib/iyzipay.ts
+++ lib/iyzipay.ts
@@ -3,7 +3,7 @@ import Iyzipay from 'iyzipay'
 const iyzipay = new Iyzipay({
     apiKey: process.env.IYZIPAY_API_KEY!,
     secretKey: process.env.IYZIPAY_SECRET_KEY!,
-    uri: process.env.IYZIPAY_URI!
+    uri: process.env.IYZIPAY_URI || 'https://sandbox-api.iyzipay.com'
 })
 
 export default iyzipay
