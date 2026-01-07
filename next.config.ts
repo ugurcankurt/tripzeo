754c5069e6d6266a3fc3d14e5ccac697ad631228
Uğur Cankurt <ugurgraphic@gmail.com>
2026-01-07 04:28:13 +0300

    fix: remove unused ts-ignore and use valid next config

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,10 @@ const nextConfig: NextConfig = {
     serverActions: {
       bodySizeLimit: '2mb',
     },
-    // @ts-expect-error outputFileTracingIncludes is a valid config but missing in some type definitions
-    outputFileTracingIncludes: {
-      '/payment/**/*': ['./node_modules/iyzipay/lib/resources/**/*'],
-      '/api/**/*': ['./node_modules/iyzipay/lib/resources/**/*'],
-    },
+  },
+  outputFileTracingIncludes: {
+    '/payment/**/*': ['./node_modules/iyzipay/lib/resources/**/*'],
+    '/api/**/*': ['./node_modules/iyzipay/lib/resources/**/*'],
   },
   images: {
     remotePatterns: [
